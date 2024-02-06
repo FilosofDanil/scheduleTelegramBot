cedaacc02e9ac7fa818d4227675a0629e70f0b16
FilosofDanil <serhio3347@gmail.com>
2024-02-06 20:16:59 +0100

    Configs. Imported several libs

---
+++ configs/configs.go
@@ -0,0 +1,40 @@
+package configs
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Configurations struct {
+	Server ServerConfigs
+	Bot    BotConfigs
+}
+
+type ServerConfigs struct {
+	Port string
+}
+
+type BotConfigs struct {
+	Name  string
+	Token string
+}
+
+var instantiated *Configurations = nil
+
+func GetInstance() *Configurations {
+	if instantiated == nil {
+		//read the configs and put it in the struct
+		viper.SetConfigName("configs")
+		viper.AddConfigPath("./configs")
+		viper.AutomaticEnv()
+		viper.SetConfigType("yml")
+		if err := viper.ReadInConfig(); err != nil {
+			fmt.Printf("Error reading config file, %s", err)
+		}
+		if err := viper.Unmarshal(&instantiated); err != nil {
+			fmt.Printf("Unable to decode into struct, %v", err)
+		}
+
+	}
+	return instantiated
+}
